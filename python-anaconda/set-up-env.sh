7e5a6642c8d510ae772f35933ecee43b80d6a2cc
vungocson <vungocson998@gmail.com>
2019-09-10 22:24:30 +0700

    Update file

--- python-anaconda/set-up-env.sh
+++ python-anaconda/set-up-env.sh
@@ -4,12 +4,22 @@
 conda env list
 
 PATH=~/your-project-dir
+ENV_NAME=my_env
 
-#Create environment, for example name = my_env
-conda create --name my_env -p $PATH python==3.7
+#Create environment with command
+conda create --name $ENV_NAME python==3.7
 
 #Activate
-conda activate my_env
+conda activate $ENV_NAME
 
 #Deactivate
 conda deactivate 
+
+#Remove environment
+conda env remove -n $ENV_NAME
+
+#Export your active environment to environment.yml
+conda env export > environment.yml
+
+#Create environment from environment.yml file
+conda env create -f environment.yml
